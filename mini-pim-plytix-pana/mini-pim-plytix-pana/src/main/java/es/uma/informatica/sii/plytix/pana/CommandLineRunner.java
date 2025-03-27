6c8b3bda1c9a1f9fc761902835f4eae49f34ba5f
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-03-27 17:56:48 +0100

    Add files via upload

---
+++ mini-pim-plytix-pana/mini-pim-plytix-pana/src/main/java/es/uma/informatica/sii/plytix/pana/CommandLineRunner.java
@@ -0,0 +1,31 @@
+package es.uma.informatica.sii.plytix.pana;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.uma.informatica.sii.plytix.pana.entities.Cuenta;
+import es.uma.informatica.sii.plytix.pana.repositories.BookRepository;
+
+@Component
+public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+	private BookRepository repository;
+	public CommandLineRunner(BookRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+
+		for (String s: args) {
+			System.out.println(s);
+		}
+
+		if (args.length > 0) {
+			for (Cuenta b: repository.findByNombre(args[0])) {
+				System.out.println(b);
+			}
+		}
+	}
+
+}
