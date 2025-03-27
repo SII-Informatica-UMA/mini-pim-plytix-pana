d5d69e7c123ba8a08ac8242feaf36c01d51b017a
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-03-27 19:15:48 +0100

    Update and rename BookRepository.java to CuentaRepository.java

--- mini-pim-plytix-pana/mini-pim-plytix-pana/src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
+++ mini-pim-plytix-pana/mini-pim-plytix-pana/src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
@@ -8,10 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import es.uma.informatica.sii.plytix.pana.entities.Cuenta;
 
-public interface BookRepository extends JpaRepository<Cuenta, Long> {
-	List<Cuenta> findByNombre(String title);
-	List<Cuenta> findByIsbnAndNombreOrderByNombreAsc(String isbn, String nombre);
-	
-	@Query("select b from Book b where b.nombre = :nombre")
-	List<Cuenta> miConsultaCompleja(@Param("nombre") String nombre);
+public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+
 }
