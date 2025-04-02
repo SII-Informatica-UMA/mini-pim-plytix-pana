82478fb6b4861ceba6e756d2132288b3144a673b
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-04-02 19:08:20 +0200

    Create PlanRepository.java

---
+++ src/main/java/es/uma/informatica/sii/plytix/pana/repositories/PlanRepository.java
@@ -0,0 +1,10 @@
+package es.uma.informatica.sii.plytix.pana.repositories;
+
+import java.util.List;
+
+import es.uma.informatica.sii.plytix.pana.entities.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+}
