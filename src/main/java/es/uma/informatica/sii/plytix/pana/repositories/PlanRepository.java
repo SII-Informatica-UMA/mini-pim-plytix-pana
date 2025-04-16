6b053f6dec343702286eef7669d71770911766fa
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-04-16 18:18:27 +0200

    Añade metodos necesarios findBy

--- src/main/java/es/uma/informatica/sii/plytix/pana/repositories/PlanRepository.java
+++ src/main/java/es/uma/informatica/sii/plytix/pana/repositories/PlanRepository.java
@@ -6,5 +6,6 @@ import es.uma.informatica.sii.plytix.pana.entities.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-
+    List<Plan> findByIdPlan(Long id);
+    List<Plan> findByNombre(String nombre);
 }
