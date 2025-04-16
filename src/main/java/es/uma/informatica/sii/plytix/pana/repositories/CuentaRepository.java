e2ccd8d1a8ccfbee469cd3ee0041edff3941d36c
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-04-16 18:16:18 +0200

    Añade metodos findBy

--- src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
+++ src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
@@ -2,7 +2,12 @@ package es.uma.informatica.sii.plytix.pana.repositories;
 
 import es.uma.informatica.sii.plytix.pana.entities.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+
+    List<Cuenta> findByNombre(String title);
+    List<Cuenta> findByIdUsuario(Long id);
+    List<Cuenta> findByIdPlan(Long id);
 
 }
