c07d755676a5e80649ceb07c0da107d1240baa6a
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-05-01 17:26:20 +0200

    Update CuentaRepository.java

--- src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
+++ src/main/java/es/uma/informatica/sii/plytix/pana/repositories/CuentaRepository.java
@@ -7,7 +7,15 @@ import java.util.*;
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
 
     List<Cuenta> findByNombre(String nombre);
-    List<Cuenta> findByIdUsuario(Long id);
-    List<Cuenta> findByIdPlan(Long id);
+
+    // Para buscar por ID de plan
+    List<Cuenta> findByPlanId(Long id);
+
+    // Para buscar cuentas donde el usuario es dueño
+    List<Cuenta> findByDuenoId(Long id);
+
+    // Para buscar cuentas que contengan un usuario en su lista
+    @Query("SELECT c FROM Cuenta c WHERE :usuarioId MEMBER OF c.usuarios")
+    List<Cuenta> findByUsuarioInUsuarios(@Param("usuarioId") Long usuarioId);
 
 }
