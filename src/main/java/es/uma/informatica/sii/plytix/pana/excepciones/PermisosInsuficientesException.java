8dba64ba7e6b79f717fc1940282ef97360d5527d
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-05-03 20:13:56 +0200

    Create PermisosInsuficientesException.java

---
+++ src/main/java/es/uma/informatica/sii/plytix/pana/excepciones/PermisosInsuficientesException.java
@@ -0,0 +1,7 @@
+package es.uma.informatica.sii.plytix.pana.excepciones;
+
+public class PermisosInsuficientesException extends RuntimeException {
+    public PermisosInsuficientesException(String message) {
+        super(message);
+    }
+}
