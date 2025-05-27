a1fcd0558a855cbf12ed3135851e055a47ab7b0d
0610912238 <124665174+0610912238@users.noreply.github.com>
2025-05-27 13:16:36 +0200

    Update UsuarioDTO.java

--- src/main/java/es/uma/informatica/sii/plytix/pana/dto/UsuarioDTO.java
+++ src/main/java/es/uma/informatica/sii/plytix/pana/dto/UsuarioDTO.java
@@ -9,9 +9,9 @@ public class UsuarioDTO {
     private Long id;
     private String email;
 
-    public UsuarioDTO(Long id){
-        this.id=id;
+    public UsuarioDTO(){
     }
+    public UsuarioDTO(Long id){this.id=id;}
     public UsuarioDTO(String email){
         Random rand = new Random();
         id = ThreadLocalRandom.current().nextLong(100);
